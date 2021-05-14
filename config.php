637cbe1932e0fc10a277ecd363521c1c56d1edda
T3IM4N <58050280+T3IM4N@users.noreply.github.com>
2021-05-14 22:15:02 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+$main_title = "Çok Yakında"; /* sitenin üst yazısı */
+
+$content_text = "Sitemiz çok yakında açılacaktır beklemede kalın."; /* sitenin alt yazısı */
+
+$webicon = "https://cdn.discordapp.com/attachments/795066298295910420/808509247485902888/2.png"; /* sitenin iconu - url */
+
+$weblabel = "Arvision | Coming Soon"; /* sitenin başlığı */
+
+$webdescription = "Sitemiz çok yakında açlılyor."; /* Sitemnin açıklaması */
+
+$webkeywords = "arvision, yazılım, php"; /* Site için anahtar kelimeler örn: arvision, yazılım, php*/
+
+$site_background = "assets/images/backgr2.jpg";
+
+$socialicon_object = '{
+    "github": {
+        "link": "https://github.com/T3IM4N",
+        "icon": "fab fa-github",
+        "color": "#cccdcf",
+        "bg_color": "#3E403C"
+    },
+    "discord": {
+        "link": "https://discord.gg/wbg8HSWucb",
+        "icon": "fab fa-discord",
+        "color": "#cccdcf",
+        "bg_color": "#7289da"
+    }
+}'; /* sitenin iconları object şeklinde ekleyebilirsiniz Not: en son object te "," bulunmamalıdır,
+object içindeki iconları https://fontawesome.com/icons sitesinden seçip sadcece class kısmının içini yapıştırın örn <i class="fab fa-accusoft"></i> bu iconun sadece fab fa-accusoft kısmını alın */
+?>
